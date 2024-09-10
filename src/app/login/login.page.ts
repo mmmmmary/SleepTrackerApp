7dba47a0080e85125d1a705a1a1729f4f6ad1ede
mmmmmary <ma.donosor@duocuc.cl>
2024-09-10 09:04:35 -0300

    commit 8?

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';  // Importa el Router para la navegación
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }  // Inyecta el Router en el constructor
 
   ngOnInit() {
   }
 
+  // Método para navegar a la página de inicio (Home)
+  goToHome() {
+    this.router.navigate(['/home']);  // Navega a la página Home
+  }
+  // Método para navegar a Loginscreen 
+  goToLoginscreen() {
+    this.router.navigate(['/loginscreen']);  // Navega a la página Home
+  }
 }
