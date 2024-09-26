314105c7ac03155382beb3802fc9bf9b25347b0d
VicenGonzalezDuoc <vicen.gonzalezl@duocuc.cl>
2024-09-26 01:43:23 -0300

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
-  {
-    path: 'loginscreen',
-    loadChildren: () => import('./loginscreen/loginscreen.module').then( m => m.LoginscreenPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  },
-  {
-    path: 'forgotpassword',
-    loadChildren: () => import('./forgotpassword/forgotpassword.module').then( m => m.ForgotpasswordPageModule)
-  },  {
-    path: 'createaccount',
-    loadChildren: () => import('./createaccount/createaccount.module').then( m => m.CreateaccountPageModule)
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -63,7 +30,6 @@ const routes: Routes = [
   {
     path: 'forgotpassword',
     loadChildren: () => import('./forgotpassword/forgotpassword.module').then( m => m.ForgotpasswordPageModule)
->>>>>>> 7e094022d27c81f259c339fb95a991b2ac5ee863
   },
   {
     path: 'perfil',
