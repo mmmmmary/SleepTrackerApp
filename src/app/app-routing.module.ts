291255ac9cef46bf8e6fa9c56f3b93fa5f1cd88f
VicenGonzalezDuoc <vicen.gonzalezl@duocuc.cl>
2024-09-26 02:36:15 -0300

    aa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,10 @@ const routes: Routes = [
     path: 'perfil',
     loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
   },
+  {
+    path: 'createaccount',
+    loadChildren: () => import('./createaccount/createaccount.module').then( m => m.CreateaccountPageModule)
+  },
 
 
 
